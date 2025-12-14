69aee23888a23462a326d76396bd92d8a180d6aa
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-14 15:31:26 +0300

    Changed data

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,65 +1,14 @@
 
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
 import './styles.css'
 
 function Homepage(){
-    
-    const popularBooks= [{
-    id: "1",
-    title: "Dune",
-    author: "Frank Herbert",
-    category: "Sci-Fi",
-    description: "Epic science fiction novel.",
-    src:"./src/Components/images/dune.jpg",
-    rating: 4.8,
-  },
-  {
-    id: "2",
-    title: "1984",
-    author: "George Orwell",
-    category: "Fiction",
-    src:"./src/Components/images/1984.jpg",
-    description: "Dystopian novel.",
-    rating: 4.7,
-  },
-                          {
-    id: "3",
-    title: "Technically yours",
-    author: "Denise williams",
-    category: "Romance",
-    src:"./src/Components/images/1984.jpg",
-    description: "Dystopian novel.",
-    rating: 4.7,
-  },
-  {
-    id: "4",
-    title: "Agatha Oddly",
-    author: "Lena Jones",
-    category: "Mystery",
-    src:"./src/Components/images/agatha.jpg",
-    description: "Dystopian novel.",
-    rating: 4.7,
-  },{
-    id: "5",
-    title: "the sun and her flowers",
-    author: "Rupi kaur",
-    category: "Poetry",
-    src:"./src/Components/images/sun.jpg",
-    description: "Dystopian novel.",
-    rating: 4.7,
-  },
-  {
-    id: "6",
-    title: "Petals of healing",
-    author: "Anne raynoe",
-    category: "Poetry",
-    src:"./src/Components/images/agatha.jpg",
-    description: "Dystopian novel.",
-    rating: 4.7,
-  }]
+  const books = useSelector((state) => state.books);
+  const popularBooks = books.filter(book => book.rating >= 4.7).slice(0, 4);
      
     return(<>
-    <div className="border-2 w-auto m-6 h-50 rounded-lg p-7 text-center text-5xl bg-amber-300">
+    <div id="wel" className="border-2 w-auto m-6 h-50 rounded-lg p-7 text-red-950 text-center text-5xl bg-amber-300">
     <h1>Welcome to OLibrary</h1>
     <h3>Nothing is pleasanter than exploring a library</h3>
     </div>
@@ -79,12 +28,13 @@ function Homepage(){
     <div className="flex flex-wrap ">
         
         {popularBooks.map((p)=>{
-           return( <div className="m-20 bg-gray-100">
+           return( <div className="m-20 bg-gray-100" key={p.id}>
            <div className="border-2 rounded-lg w-60 h-80 bg-gray-100 hover:scale-110" key={p.id}>
             
             <img src={p.src} className="w-70 h-55"></img>
             <h2 className="text-xl font-bold text-blue">{p.title}</h2>
             <h2 className="text-xl text-blue">Author:{p.author}</h2>
+            <Link to={`/viewdetails/${p.id}`} className="underline text-red-500">View Details</Link>
             </div>
             </div>)
         })}
