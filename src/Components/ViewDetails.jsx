a19a7889dc61dc47dc139fc89880ef87042f2d33
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-14 05:03:23 +0300

    Updated component

--- src/Components/ViewDetails.jsx
+++ src/Components/ViewDetails.jsx
@@ -1,4 +1,28 @@
-function ViewDetails(){
+import { useParams, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+function ViewDetails() {
+  const { id } = useParams();
+
+   const book = useSelector(state =>
+    state.books.find(b => b.id === id)
+  );
+
+  if (!book) return <p>Book not found</p>;
+
+  return (
+    <>
+      <h2>{book.title}</h2>
+      <p>Author: {book.author}</p>
+      <p>{book.description}</p>
+      <p>Rating: {book.rating}</p>
+
+      <Link to={`/browsebooks/${book.category}`}>Back to Browse</Link>
+    </>
+  );
 }
-export default ViewDetails
+
+export default ViewDetails;
+
+  
+   
