24177e6bdcd9f444bec325ce04e165ed8586b314
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-15 10:08:09 +0300

    added comments

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom"
 import Navigation from "./Navigation"
 
+
+//Navigation bar and outlet for changing pages
 function Header(){
     return(<>
     <Navigation></Navigation>
