5e0903920991c820539642a063f7f57947392bdb
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-15 09:55:32 +0300

    added image input

--- src/Components/AddBooks.jsx
+++ src/Components/AddBooks.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 function AddBooks() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  
 
   const [form, setForm] = useState({
     title: "",
@@ -31,7 +32,7 @@ function AddBooks() {
     <div className="flex flex-row w-110 border-2 rounded-lg bg-amber-100 ml-100 mt-5">
     <form onSubmit={handleSubmit} >
       <span className="text-xl font-bold ml-6">Title:</span><input className="border-2 rounded-lg m-5" placeholder="Title" onChange={e => setForm({...form, title: e.target.value})} /><br></br>
-      <span className="text-xl font-bold ml-6">Author:</span><input className="border-2 rounded-lg m-5" placeholder="Author" onChange={e => setForm({...form, author: e.target.value})} /><br></br>
+      <span className="text-xl font-bold ml-6">Author:</span><input className="border-2 rounded-lg m-5" placeholder="Author"  onChange={e => setForm({...form, author: e.target.value})} /><br></br>
       <span className="text-xl font-bold ml-5">Category:</span><select className="border-2 rounded-lg m-5" onChange={e => setForm({...form, category: e.target.value})}>
   <option selected>Fiction</option>
   <option>Non-Fiction</option>
@@ -43,10 +44,28 @@ function AddBooks() {
       <span className="text-xl font-bold ml-5">Image:</span><input className="border-2 rounded-lg m-5" placeholder="imageurl" onChange={e => setForm({...form, src: e.target.value})} />
       <h2 className="text-xl font-bold ml-5">Description:</h2><textarea className="border-2 rounded-lg m-5 h-20 w-50" placeholder="Description" onChange={e => setForm({...form, description: e.target.value})} /><br></br>
       <span className="text-xl font-bold ml-5">Rating:</span><input className="border-2 rounded-lg m-5" type="number" placeholder="Rating" onChange={e => setForm({...form, rating: e.target.value})} /><br></br>
+      <h4>Image Preview</h4>
+      {form.src && (
+          <img
+            src={form.src}
+            alt="Preview"
+            className="w-40 h-40 object-cover rounded mt-2 border"
+            onError={(e) => {
+              e.target.src = "/images/default.png"; 
+            }}
+            />
+        )}
+      
+      
+      
+      
       <button className="border-2 font-semibold rounded-lg m-5 w-20 bg-amber-500 hover:scale-110">Add Book</button>
     </form>
+    
     </div>
+
   );
+  
 }
 
 
